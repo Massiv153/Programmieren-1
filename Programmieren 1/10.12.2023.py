a27a6a1648fdd2da28547fc79e1d6df6c2de1669
Massiv153 <101190668+Massiv153@users.noreply.github.com>
2023-12-10 20:27:00 +0000

    tägliches Programmieren

---
+++ Programmieren 1/10.12.2023.py
@@ -0,0 +1,4 @@
+class Zinsrechner:
+    kapital=(float)(input("Kapital: "))
+    zinsen=(float)(input("Zinsen in Prozent: "))
+    print("Ihr Kapital mit Zinsen beträgt "+(str)(kapital+(kapital*zinsen/100))+" Euro")
