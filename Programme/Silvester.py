f91a5252cb35e6df49df3f243543dbbd1567e9a1
Massiv153 <101190668+Massiv153@users.noreply.github.com>
2023-12-31 18:34:51 +0000

    Tägliches Programmieren 31.12.23: Silvester Programm

---
+++ Programme/Silvester.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    for e in range (i,10+i):
+        if(e%2==0):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print("")
+print ("Frohes Neues")
