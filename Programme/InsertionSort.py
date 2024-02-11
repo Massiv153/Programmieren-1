8bc44873bdfafd2f891984b180e204fdd940579e
Massiv153 <101190668+Massiv153@users.noreply.github.com>
2024-02-11 21:08:11 +0000

    Tägliches Programmieren 11.2.24

--- Programme/InsertionSort.py
+++ Programme/InsertionSort.py
@@ -1,5 +1,8 @@
 def insertionSort(l):
     a=list()
     a.append(l[0])
-    for i in range(1:len(l)):
+    for i in range(1,len(l)):
+        for e in a:
+            if l[i]>=e:
+                a.insert(i,l[i])
     
